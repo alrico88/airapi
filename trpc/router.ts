f2a83798d888a932ac38ad6b42be79e3a2a5d233
Alberto Rico <alrico88@gmail.com>
2024-01-14 22:25:54 +0100

    feat: add typeahead route

--- trpc/router.ts
+++ trpc/router.ts
@@ -1,8 +1,10 @@
 import { router } from './trpc';
 import { airportRouter } from './routers/airport';
+import { typeaheadRouter } from './routers/typeahead';
 
 export const appRouter = router({
   airport: airportRouter,
+  typeahead: typeaheadRouter
 });
 
 // export type definition of API
