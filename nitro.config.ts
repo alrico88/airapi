f0bf80ce37323f6a136d01ff7c5ca0cbb1df8ca5
Alberto Rico <alrico88@gmail.com>
2024-02-27 01:22:19 +0100

    feat: cors

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,6 +1,14 @@
 // https://nitro.unjs.io/config
 export default defineNitroConfig({
   runtimeConfig: {
-    baseUrl: '/api'
-  }
+    baseUrl: '/api',
+  },
+  routeRules: {
+    '**': {
+      cors: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    },
+  },
 });
